c21992d8ec86619e8cbb3118e68d9d9fd3971013
LizClaudia <20203tn128@utez.edu.mx>
2023-07-04 20:57:24 -0600

    Ultimos cambios

--- function.js
+++ function.js
@@ -1,7 +1,7 @@
 console.log("Hola desde function.js")
 
 let producto={nombre:"Ricardo",precio:0.0,cantidad:0, total:()=>{ precio * cantidad}}
-const productos ={};
+let productos =[];
 
 let carrito ={producto,folio:'',subtotal:()=>{producto.total},total:()=>{carrito.subtotal*0.16}}
 
@@ -27,7 +27,7 @@ function agregar(){
  producto.precio = precio.value;
  producto.cantidad = cantidad.value;
  console.log(producto);
- producto.push(producto);
+ productos.push(producto);
  console.log(productos)
 }
 
@@ -40,20 +40,4 @@ function cerrar(){
 }
 
 
-function  turnOn(){
-    if(tv.power){
-        console.log("A encendido el televisor");
-        tv.volumen = 3;
-        tv.canal = 7;
-        tv.power = false;
-        console.log(tv)
-    }else{
-        console.log("A apagado el televisor");
-        tv.power = true;
-        tv.volumen = 0;
-        tv.canal= 0;
-        tv.silence = false;
-        console.log(tv)
 
-    }
-}
